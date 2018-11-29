debc09351876c1491719ef604f0bea34c8585cdc
Junior2Ran <hdr01@126.com>
2018-11-29 12:04:17 +0800

    drop console.error in postmessage

--- src/app/datas/datasCore.vue
+++ src/app/datas/datasCore.vue
@@ -29,7 +29,7 @@ export default {
         });
 
         window.addEventListener('message', e => {
-            if (/\.baidu\.com/.test(e.origin)) {
+            if (/\.baidu\.com/.test(e.origin) && e.data) {
                 let data = JSON.parse(e.data);
                 e.source.postMessage('received data', e.origin);
                 Action.home.emit('importData');
